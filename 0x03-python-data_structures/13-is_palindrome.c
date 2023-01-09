1bfe56d4092d430a4340a4304c09611ee7a2b8f7
ejyrose <jyko2017@gmail.com>
2023-01-09 03:15:14 -0800

    13-is_palindrome.c_updated

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,21 +9,70 @@
  */
 int is_palindrome(listint_t **head)
 {
-	int len = 0, num[4500], pal = 1, i = 0;
-	listint_t *copyh = *head;
+	listint_t *start = NULL, *end = NULL;
+	unsigned int i = 0, len = 0, len_cyc = 0, len_list = 0;
 
-	if (!*head || !((*head)->next))
+	if (head == NULL)
+		return (0);
+
+	if (*head == NULL)
 		return (1);
 
-	for (i = 0; copyh; i++, copyh = copyh->next)
-		num[i] = copyh->n;
-	for (len = i, i = 0; i < len; i++)
+	start = *head;
+	len = listint_len(start);
+	len_cyc = len * 2;
+	len_list = len_cyc - 2;
+	end = *head;
+
+	for (; i < len_cyc; i = i + 2)
 	{
-		if (num[i] != num[len - 1 - i])
+		if (start[i].n != end[len_list].n)
+			return (0);
+
+		len_list = len_list - 2;
+	}
+	return (1);
+}
+/**
+ * get_nodeint_at_index - Gets a node from a linked list
+ * @head: The head of the linked list
+ * @index: The index to find in the linked list
+ *
+ * Return: The specific node of the linked list
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *current = head;
+	unsigned int iter_times = 0;
+
+	if (head)
+	{
+		while (current != NULL)
 		{
-			pal = 0;
-			break;
+			if (iter_times == index)
+				return (current);
+
+			current = current->next;
+			++iter_times;
 		}
 	}
-	return (pal);
+	return (NULL);
+}
+/**
+ * slistint_len - Counts the number of elements in a linked list
+ * @h: The linked list to count
+ *
+ * Return: Number of elements in the linked list
+ */
+size_t listint_len(const listint_t *h)
+{
+	int lenght = 0;
+
+	while (h != NULL)
+	{
+		++lenght;
+		h = h->next;
+	}
+	
+	return (lenght);
 }
