27ee2cb4742d13c87c1915a35fd4731e8524c5f1
ejyrose <jyko2017@gmail.com>
2023-01-12 13:25:01 -0800

    103-python.cadvance

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,12 @@
+#include "/usr/include/python3.4/Python.h"
+#include <stdio.h>
+
+void print_hexn(const char *str, int n)
+{
+	int i = 0;
+
+	for (; i < n - 1; ++i)
+		printf("%02x ", (unsigned char) str[i]);
+
+	printf("%02x", str[i]);
+}
