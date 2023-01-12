c310fa0a1ca52309655db73e2d4ae886bbce7976
ejyrose <jyko2017@gmail.com>
2023-01-12 13:40:59 -0800

    8simpledelete_updated

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def simple_delete(my_dict, key=""):
-    if key in my_dict:
-        del my_dict[key]
-        return my_dict
+def simple_delete(a_dictionary, key=""):
+    a_dictionary.pop(key, None)
+    return a_dictionary
